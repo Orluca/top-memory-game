e7573fcc9c921e7b5aa9435c06cd092b56f08ded
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-09-26 16:00:58 +0200

    Global styles with index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Test from "./components/Test";
-import GlobalStyles from "./components/General/GlobalStyle";
 import Scoreboard from "./components/Scoreboard/Scoreboard";
 
 function App() {
   return (
     <div className="App">
-      <GlobalStyles />
       <Header />
       {/* <Test /> */}
       <Scoreboard />

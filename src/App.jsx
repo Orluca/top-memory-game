60f627ac2a353d2f0aff28db6c95a70d51287d46
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-09-26 20:06:00 +0200

    Store score as state

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import React, { useState } from "react";
 
 function App() {
   const [cardAmount, setCardAmount] = useState(4);
+  const [score, setScore] = useState(0);
 
   function handleClick() {
     setCardAmount(8);
@@ -14,7 +15,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Scoreboard />
+      <Scoreboard score={score} />
       <Gameboard cardAmount={cardAmount} />
       <button onClick={handleClick}>More cards</button>
     </div>
--- src/components/Scoreboard/Scoreboard.jsx
+++ src/components/Scoreboard/Scoreboard.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import Score from "./Score/Score";
 import ScoreboardStyled from "./Scoreboard.styled";
 
-function Scoreboard() {
+function Scoreboard(props) {
   return (
     <ScoreboardStyled>
-      <Score name="Score" value="5" />
+      <Score name="Score" value={props.score} />
       <Score name="Best" value="10" />
     </ScoreboardStyled>
   );

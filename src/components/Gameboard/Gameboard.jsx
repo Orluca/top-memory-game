29f713d511e5e2b41f096080ffa9d2e46dbdeac6
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-09-26 17:08:36 +0200

    random cards generator

--- src/components/Gameboard/Gameboard.jsx
+++ src/components/Gameboard/Gameboard.jsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GameboardStyled from "./Gameboard.styled";
 import MemoryCard from "./MemoryCard/MemoryCard";
+import { v4 as uuidv4 } from "uuid";
 
 function Gameboard() {
-  return (
-    <GameboardStyled>
-      <MemoryCard id="1" />
-      <MemoryCard id="11" />
-      <MemoryCard id="111" />
-      <MemoryCard id="54" />
-      <MemoryCard id="58" />
-      <MemoryCard id="98" />
-    </GameboardStyled>
-  );
+  const [cards, setCards] = useState([]);
+
+  function createRandomNumber() {
+    return Math.floor(Math.random() * 826) + 1;
+  }
+
+  function randomNumbers(amount) {
+    const emptyArray = new Array(amount).fill(NaN);
+    const numbers = emptyArray.map((el) => createRandomNumber());
+    console.log(numbers);
+    return numbers;
+  }
+
+  useEffect(() => {
+    const randomCards = randomNumbers(4).map((id) => <MemoryCard id={id} key={uuidv4()} />);
+
+    setCards(randomCards);
+  }, []);
+
+  return <GameboardStyled>{cards}</GameboardStyled>;
 }
 
 export default Gameboard;

42834a9368284cb1fc2cc996fc83c02317379138
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-09-28 00:16:25 +0200

    Update Gameboard.jsx

--- src/components/Gameboard/Gameboard.jsx
+++ src/components/Gameboard/Gameboard.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GameboardStyled from "./Gameboard.styled";
 
 function Gameboard() {
-  return <></>;
+  const [characters, setCharacters] = useState([]);
+
+  return <GameboardStyled></GameboardStyled>;
 }
 
 export default Gameboard;

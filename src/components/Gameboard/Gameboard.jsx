4b0a7f6dfebe0ec0cecf7ac3f47c6c88bb46850b
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-09-28 09:48:49 +0200

    filter out double chars

--- src/components/Gameboard/Gameboard.jsx
+++ src/components/Gameboard/Gameboard.jsx
@@ -4,7 +4,38 @@ import GameboardStyled from "./Gameboard.styled";
 function Gameboard() {
   const [characters, setCharacters] = useState([]);
 
-  return <GameboardStyled></GameboardStyled>;
+  async function getCharacterData(id) {
+    return fetch(`https://rickandmortyapi.com/api/character/${id}`)
+      .then((response) => response.json())
+      .then((data) => data);
+  }
+
+  function createCharacter(id) {
+    getCharacterData(id).then((data) =>
+      setCharacters((prev) => {
+        const filteredPrev = prev.filter((char) => char.id !== id);
+        return [...filteredPrev, { name: data.name, id: data.id, image: data.image }];
+      })
+    );
+  }
+
+  function generateCards(amount) {
+    const emptyArray = new Array(amount).fill(NaN);
+  }
+
+  useEffect(() => {
+    createCharacter(1);
+  }, []);
+
+  function handleClick() {
+    console.log(characters);
+  }
+
+  return (
+    <GameboardStyled>
+      <button onClick={handleClick}>btn</button>
+    </GameboardStyled>
+  );
 }
 
 export default Gameboard;
